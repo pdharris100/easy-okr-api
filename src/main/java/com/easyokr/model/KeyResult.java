35eef3dc9a020768a5649c5e5827ed57a995c764
Paul <pdharris@gmail.com>
2019-11-06 21:20:40 +0100

    Prep for BitBucket pipelines

--- src/main/java/com/easyokr/model/KeyResult.java
+++ src/main/java/com/easyokr/model/KeyResult.java
@@ -1,5 +1,7 @@
 package com.easyokr.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,6 +25,8 @@ public class KeyResult {
 	private long baseline;
 	private long target;
 	private long current;
+	private Date start;
+	private Date deadline;
 	
 	protected KeyResult () {}
 	
